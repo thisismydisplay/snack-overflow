32b28e25a268228b9281b4d60b613f2c02010edf
haywood-d-johnson <haywood.d.johnson@gmail.com>
2022-04-14 15:26:11 -0500

    feat: button styling

--- public/javascripts/votes.js
+++ public/javascripts/votes.js
@@ -1,28 +1,26 @@
-
-    //check for undefined votes and set to 0
-    const voteSpans = document.querySelectorAll(".vote-span");
-    const numAnswerSpans = document.querySelectorAll(".num-answer-span")
-    document.addEventListener("DOMContentLoaded", async () => {
-        numAnswerSpans.forEach((numAnswerSpan) => {
-            if (numAnswerSpan.innerText === "undefined") {
-                numAnswerSpan.innerText = "0";
-            }
-            if (numAnswerSpan.innerText == "1"){
-                let identifier = numAnswerSpan.id;
-                let num = identifier.slice(4);
-                let el = document.querySelector(`.num-answer-label${num}`)
-                el.innerText = ' Answer'
-            }
-        });
-        voteSpans.forEach((voteSpan) => {
-            if (voteSpan.innerText === "undefined") {
-                voteSpan.innerText = "0";
-            }
-        });
-        const answerVotesEle = document.querySelector('.num-answers')
-
-                if (answerVotesEle.innerText === "1 Answers") {
-                    answerVotesEle.innerText = '1 Answer'
-                }
-
+//check for undefined votes and set to 0
+const voteSpans = document.querySelectorAll(".vote-span");
+const numAnswerSpans = document.querySelectorAll(".num-answer-span");
+document.addEventListener("DOMContentLoaded", async() => {
+    numAnswerSpans.forEach((numAnswerSpan) => {
+        if (numAnswerSpan.innerText === "undefined") {
+            numAnswerSpan.innerText = "0";
+        }
+        if (numAnswerSpan.innerText == "1") {
+            let identifier = numAnswerSpan.id;
+            let num = identifier.slice(4);
+            let el = document.querySelector(`.num-answer-label${num}`);
+            el.innerText = " Answer";
+        }
     });
+    voteSpans.forEach((voteSpan) => {
+        if (voteSpan.innerText === "undefined" || voteSpan.innerText === "") {
+            voteSpan.innerText = "0";
+        }
+    });
+    const answerVotesEle = document.querySelector(".num-answers");
+
+    if (answerVotesEle.innerText === "1 Answers") {
+        answerVotesEle.innerText = "1 Answer";
+    }
+});
