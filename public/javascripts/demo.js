45d23c72332d12fc5837c2fac2c10c5ad9a609a7
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-04-15 03:32:20 -0400

    Added fully functioning demo user - minor error with login rendering

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -1,7 +1,14 @@
-window.addEventListener("onLoad", async (event) => {
-    const demoBtn = document.getElementById('demo-btn')
-    demoBtn.addEventListener('click', async (event) => {
-        document.getElementById('demo-btn').innerHTML = 'Loading...'
+window.addEventListener("DOMContentLoaded", async (event) => {
+    const demoBtn = document.getElementsByClassName('demo-btn')[0];
 
-    })
-})
+    demoBtn.addEventListener('click', async (event) => {
+        console.log('CLICKED DEMO BUTTON');
+        document.getElementById('login-email').value = 'leo@leo.com';
+        document.getElementById('login-password').value = 'password';
+        // document.getElementById('login-submit').click();
+        document.querySelector('.login-form').submit();
+
+    });
+});
+// const response = await res.json();
+// document.getElementById('demo-btn').innerHTML = 'Loading...'
